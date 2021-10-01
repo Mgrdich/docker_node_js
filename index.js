fbdb441fa1cb7b1b3be9cb8dd9bb6b8760036006
Mgo <mgotm13@gmail.com>
2021-10-02 01:44:22 +0400

    node js server code bug fix during the run up time page request

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get('/',function(req,res){
-    req.send('Hi There');
+    res.send('Hi There');
 });
 
 app.listen(8080,function(){
